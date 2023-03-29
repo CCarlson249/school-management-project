380f7fb0431c67a501e8a9135bd7b8e3c8ab05bd
Christian Hernandez <chernandez148@DESKTOP-3F6UKNT.localdomain>
2023-03-29 15:35:29 -0700

    updated

--- lib/cli.py
+++ lib/cli.py
@@ -1,45 +1,34 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from helpers import add_student, add_teacher, add_grade, get_student_classes
+from helpers import admin_page, teacher_page, student_page
 from db.models import Base, Students, Teachers, Grades
 
 engine = create_engine('sqlite:///school.db')
 Base.metadata.create_all(engine)
-session = sessionmaker(bind=engine)()
+Session = sessionmaker(bind=engine)
 
+
+page = 1
 
 while True:
-    command = input("Insert command: ")
-    if command == "Add new Student":
-        first_name = input("Enter first name: ")
-        last_name = input("Enter last name: ")
-        add_student(first_name, last_name)
 
-    elif command == "Add new Teacher":
-        first_name = input("Enter first name: ")
-        last_name = input("Enter last name: ")
-        subject = input("Enter subject: ")
-        add_teacher(first_name, last_name, subject)
+    if page == 1:
+        select = int(input(f'''
+        Please select who you are:
+        
+        1 - Admin
+        2 - Teachers
+        3 - Students
+        4 - Log Out
 
-    elif command == "Add new Grade":
-        teacher_id = input("Enter teacher ID: ")
-        student_id = input("Enter student ID: ")
-        grade = input("Enter grade: ")
-        add_grade(teacher_id, student_id, grade)
-
-    elif command == "search student":
-        first_name = input("Enter student first name: ")
-        class_info = get_student_classes(first_name)
-        if class_info:
-            print(f"Classes for {first_name}:")
-            for subject, grade in class_info:
-                print(f"{subject}: {grade}")
+        ENTER: '''))
+        if select == 1:
+            admin_page()
+        elif select == 2:
+            teacher_page()
+        elif select == 3:
+            student_page()
+        elif select == 4:
+            Session.close()
         else:
-            print("No classes found.")
-
-    elif command == "Quit":
-        session.close()
-        break
-
-    else:
-        print("Invalid command. Please try again.")
+            print("Error, invalid selecton.")
--- lib/helpers.py
+++ lib/helpers.py
@@ -1,21 +1,101 @@
 from db.models import Students, Teachers, Grades
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import IntegrityError
+
 
 engine = create_engine('sqlite:///school.db')
 Session = sessionmaker(bind=engine)
 
+# ------------------ Admin Page --------------------- #
 
-def add_student(first_name, last_name):
-    session = Session()
-    new_student = Students(first_name=first_name, last_name=last_name)
-    session.add(new_student)
-    session.commit()
-    student_id = new_student.id
-    student_name = f"{new_student.first_name} {new_student.last_name}"
-    session.close()
-    print(f"Added new student: {student_name} (ID: {student_id})")
-    return student_id, student_name
+
+def admin_page():
+    select = int(input(f'''
+        Please select:
+
+        1 - Add Teachers
+        2 - View All Teachers
+
+    ENTER: '''))
+
+    # --------------- Add Teacher ------------------- #
+
+    if select == 1:
+
+        first_name = input("Enter first name: ")
+        last_name = input("Enter last name: ")
+        subject = input("Enter subject: ")
+        add_teacher(first_name, last_name, subject)
+
+    # ------------- View All Teachers --------------- #
+
+    if select == 2:
+        view_all_teachers()
+
+
+# ----------------- Teacher Page --------------------- #
+
+
+def teacher_page():
+    select = int(input(f'''
+        Please select:
+        
+        1 - Add Student
+        2 - Add Grade
+        3 - View Grades
+        4 - Drop student
+
+    ENTER: '''))
+
+    # -------------- Add Student ------------- #
+
+    if select == 1:
+
+        first_name = input("Enter first name: ")
+        last_name = input("Enter last name: ")
+        add_student(first_name, last_name)
+
+    # -------------- Add Grade --------------- #
+
+    if select == 2:
+
+        teacher_id = input("Enter teacher ID: ")
+        student_id = input("Enter student ID: ")
+        grade = input("Enter grade: ")
+        add_grade(teacher_id, student_id, grade)
+
+    # ----------- View Student Grades ----------#
+
+    if select == 3:
+
+        teacher_id = input("Enter teacher ID: ")
+        view_all_grades(teacher_id)
+
+    # ----------- Drop Student ----------------#
+
+    if select == 4:
+
+        student_id = input("Enter student ID: ")
+        drop_student(student_id)
+
+# ------------- Student Page ----------------- #
+
+
+def student_page():
+    select = int(input(f'''
+        Please select:
+        
+        1 - View Classes
+        2 - View Grades
+        3 - Drop Class
+
+    ENTER: '''))
+
+    # --------- View Students Classes ---------- #
+
+    if select == 1:
+        pass
 
 
 def add_teacher(first_name, last_name, subject):
@@ -29,6 +109,37 @@ def add_teacher(first_name, last_name, subject):
     session.close()
     print(f"Added new teacher: {teacher_name} (ID: {teacher_id})")
     return teacher_id, teacher_name
+
+
+def view_all_teachers():
+    session = Session()
+    try:
+        # query for all teachers
+        teachers = session.query(Teachers).all()
+        if teachers:
+            # print the teachers to the console
+            for teacher in teachers:
+                print(
+                    f"{teacher.id}: {teacher.first_name} {teacher.last_name} - {teacher.subject}")
+        else:
+            print("No teachers found")
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        session.close()
+
+
+def add_student(first_name, last_name):
+    session = Session()
+    new_student = Students(
+        first_name=first_name, last_name=last_name)
+    session.add(new_student)
+    session.commit()
+    student_id = new_student.id
+    student_name = f"{new_student.first_name} {new_student.last_name}"
+    session.close()
+    print(f"Added new student: {student_name} (ID: {student_id})")
+    return student_id, student_name
 
 
 def add_grade(teacher_id, student_id, grade):
@@ -46,18 +157,59 @@ def add_grade(teacher_id, student_id, grade):
     return grade_id, teacher_id, student_id, grade
 
 
-def get_student_classes(first_name):
+def view_all_grades(teacher_id):
     session = Session()
-    student = session.query(Students).filter_by(first_name=first_name).first()
-    if not student:
-        print(f"No student found with first name '{first_name}'.")
+    try:
+        # query for the teacher object
+        teacher = session.query(Teachers).get(teacher_id)
+        if teacher:
+            # query for all grades associated with the teacher
+            grades = session.query(Grades).filter_by(
+                teachers_id=teacher_id).all()
+            if grades:
+                # group the grades by student
+                grades_by_student = {}
+                for grade in grades:
+                    student_id = grade.students_id
+                    student = session.query(Students).get(student_id)
+                    if student:
+                        if student not in grades_by_student:
+                            grades_by_student[student] = []
+                        grades_by_student[student].append(grade.grade)
+                # print the grades to the console
+                for student, grades in grades_by_student.items():
+                    print(
+                        f"{teacher.first_name} {teacher.last_name} - {student.first_name} {student.last_name}: {grades}")
+            else:
+                print(f"No grades found for teacher with id {teacher_id}")
+        else:
+            print(f"No teacher found with id {teacher_id}")
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
         session.close()
-        return []
 
-    grades = student.grades
-    class_info = []
-    for grade in grades:
-        teacher = grade.teachers
-        class_info.append((teacher.subject, grade.grade))
-    session.close()
-    return class_info
+
+def drop_student(student_id):
+    session = Session()
+    try:
+        # Delete all grades associated with the student
+        session.query(Grades).filter_by(students_id=student_id).delete()
+        session.commit()
+
+        # Delete student from the students table
+        student = session.query(Students).get(student_id)
+        if student:
+            session.delete(student)
+            session.commit()
+            return True
+        else:
+            return False
+    except IntegrityError as e:
+        session.rollback()
+        print(f"Error: {e}")
+        return False
+    finally:
+        session.close()
+
+# -------------------- Student's Functions -------------------- #
